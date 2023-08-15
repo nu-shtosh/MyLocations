0ee332418eb239dcfc0c3ed7a990651a93b5d008
ILIA <dubenskiiilia@gmail.com>
2023-08-15 14:40:15 +0300

    Initial Commit

---
+++ LocationsApp/LocationsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationsAppApp.swift
+//  LocationsApp
+//
+//  Created by Илья Дубенский on 15.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct LocationsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
